baa8e8bb3cf837a760c731678e03ef6029393bd0
daniel <daniel@172.21.21.214>
2013-02-28 16:38:18 +0100

    working/testing parser

--- src/real/Objects/Parser/ExpressionParser.java
+++ src/real/Objects/Parser/ExpressionParser.java
@@ -69,7 +69,6 @@ public class ExpressionParser
                 
                 else
                 {
-                    tokenStream.consume();
                     list.add(primary());
                     break;
                 }
