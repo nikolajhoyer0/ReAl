9acb5cb438e58d82f9b88a82f5d79905a6609cf9
Daniel Gavin <danielgavin4@msn.com>
2013-03-09 22:14:03 +0100

    made sure that datamanager throws exception if there is a duplicate

---
+++ src/real/Objects/Exceptions/DatasetDuplicate.java
@@ -0,0 +1,14 @@
+package real.Objects.Exceptions;
+
+public class DatasetDuplicate extends Exception
+{
+    public DatasetDuplicate()
+    {
+        
+    }
+    
+    public DatasetDuplicate(String message)
+    {
+        super(message);
+    }  
+}
