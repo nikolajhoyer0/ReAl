610bb4c5a0b6ee0c99363a526c56b8b79659d17a
Nikolaj <nikolajhoyer@gmail.com>
2013-03-11 13:10:53 +0100

    difference operation implemented

--- src/real/Objects/RAOperations/Difference.java
+++ src/real/Objects/RAOperations/Difference.java
@@ -1,12 +1,17 @@
 
 package real.Objects.RAOperations;
 
+import java.util.ArrayList;
 import real.BaseClasses.BinaryOperationBase;
 import real.BaseClasses.OperationBase;
 import real.Objects.Dataset;
 import real.Objects.Exceptions.InvalidSchema;
 import real.Objects.Exceptions.NoSuchDataset;
+import real.Objects.Row;
 
+/**
+ * Implements the difference operation on bags.
+ */
 public class Difference extends BinaryOperationBase
 {
 
@@ -18,6 +23,22 @@ public class Difference extends BinaryOperationBase
     @Override
     public Dataset execute() throws InvalidSchema, NoSuchDataset
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Dataset resultA = this.operandA.execute().clone();
+        Dataset resultB = this.operandB.execute().clone();
+        
+        if (resultA.equalsSchema(resultB))
+        {
+            for (Row row : resultA.getRows())
+            {
+                if (resultB.getRows().contains(row))
+                {
+                    resultA.getRows().remove(row);
+                    resultB.getRows().remove(row);
+                }
+            }
+            return new Dataset("", resultA.getColumns(), resultA.getRows());
+        }
+        
+        throw new InvalidSchema(); 
     }
 }
