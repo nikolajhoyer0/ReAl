a1f55ab217a1bfbea0aa346735d1ea6a43b0a1c2
Nikolaj <nikolajhoyer@gmail.com>
2013-03-15 13:17:53 +0100

    added some code to save button

--- src/real/Objects/Services/MainWindow.java
+++ src/real/Objects/Services/MainWindow.java
@@ -547,6 +547,16 @@ public class MainWindow extends javax.swing.JFrame implements IService
     private void saveButtonActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_saveButtonActionPerformed
     {//GEN-HEADEREND:event_saveButtonActionPerformed
         JOptionPane.showMessageDialog(rootPane, "Feature not implemented yet");
+// TODO: figure out how to implement
+//        String name = queryView.getTitleAt(queryView.getSelectedIndex());
+//        try {
+//            Dataset dataset = Kernel.GetService(DataManager.class).getDataset(name);
+//            Kernel.GetService(DataManager.class).setDataset(dataset);
+//            relationModel.addElement(name);
+//            TextQueryView.addTableAutoWords(name);
+//        } catch (NoSuchDataset ex) {
+//            System.out.println("No such dataset");
+//        }
     }//GEN-LAST:event_saveButtonActionPerformed
 
     private void runButtonActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_runButtonActionPerformed
